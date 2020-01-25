0b4ba94318b8a3e7e58f16f6ac0ee4ace8366781
Venkata Reddy Vazrala <vazralavenkatareddy@gmail.com>
2020-01-24 21:52:51 -0500

    angular page with images

--- app/phone-list.component.js
+++ app/phone-list.component.js
@@ -2,24 +2,14 @@ angular.
   module('phonecatApp').
   component('phoneList', { 
     templateUrl:'template.html',
-    controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nokia',
-          snippet: 'Fast just got faster with Nexus S.',
-          age:2
-        }, 
-        {
-          name:'phone',
-          snippet: 'The Next, Next Generation tablet.',
-          age:1
-        }, 
-        {
-          name: 'Pixel',
-          snippet: 'The Next, Next Generation tablet.',
-          age:3
-        }
-      ];
-      this.orderProp='age';
-    }
+    controller: ['$http',
+      function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
+
+        $http.get('phones/phones.json').then(function(response) {
+          self.phones = response.data;
+        });
+      }
+    ]
   });
